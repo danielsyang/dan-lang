7589d24867d7eb8595be0e436ae1f8ba83699ba5
Daniel Yang <shu.minyang@hotmail.com>
2023-09-08 23:25:36 -0400

    refactored til grouped expression

--- src/ast/parser.rs
+++ src/ast/parser.rs
@@ -5,7 +5,7 @@ use crate::lex::{
     token::{Token, TokenType},
 };
 
-use super::tree::{Expression, Literal, Program, Statement};
+use super::tree::{Expression, Literal, Operator, Prefix, Program, Statement};
 
 #[derive(Clone, Copy, Debug)]
 enum Precedence {
@@ -139,17 +139,43 @@ impl Parser {
         Statement::Return(return_val)
     }
 
-    fn parse_expression(&mut self, precedence: Precedence) -> Expression {
-        match &self.current_token.kind {
+    fn parse_expression(&mut self, p: Precedence) -> Expression {
+        let mut left_exp = match &self.current_token.kind {
             TokenType::Int(v) => Expression::Literal(Literal::Number(*v)),
             TokenType::Identifier => Expression::Identifier(self.current_token.literal.clone()),
             TokenType::String(s) => Expression::Literal(Literal::String(s.clone())),
             TokenType::Boolean(b) => Expression::Literal(Literal::Boolean(*b)),
+            TokenType::BangSign => self.parse_prefix_expression(Prefix::Bang),
+            TokenType::MinusSign => self.parse_prefix_expression(Prefix::Minus),
+            TokenType::LeftParen => self.parse_grouped_expression(),
             _ => panic!(
                 "parse_expression: not yet implemented, got {:?}",
                 self.current_token.kind
             ),
+        };
+
+        while (p as u8) < self.next_precedence() && self.next_token.kind != TokenType::Semicolon {
+            left_exp = match self.next_token.kind {
+                TokenType::PlusSign => self.parse_infix_expression(left_exp, Operator::Plus),
+                TokenType::MinusSign => self.parse_infix_expression(left_exp, Operator::Minus),
+                TokenType::MultiplicationSign => {
+                    self.parse_infix_expression(left_exp, Operator::Multiply)
+                }
+                TokenType::SlashSign => self.parse_infix_expression(left_exp, Operator::Divide),
+                TokenType::Eq => self.parse_infix_expression(left_exp, Operator::Equal),
+                TokenType::NotEq => self.parse_infix_expression(left_exp, Operator::NotEqual),
+                TokenType::LT => self.parse_infix_expression(left_exp, Operator::LessThan),
+                TokenType::GT => self.parse_infix_expression(left_exp, Operator::GreaterThan),
+                TokenType::LeftParen => {
+                    self.consume_token();
+                    // self.parse_call_expression(left_exp)
+                    todo!("self.parse_call_expression(left_exp)")
+                }
+                _ => left_exp,
+            };
         }
+
+        left_exp
     }
 
     fn parse_expression_statement(&mut self) -> Statement {
@@ -160,6 +186,66 @@ impl Parser {
         }
 
         Statement::Expression(exp)
+    }
+
+    fn parse_infix_expression(&mut self, left: Expression, op: Operator) -> Expression {
+        self.consume_token();
+        let precedence = self.current_precedence();
+        self.consume_token();
+
+        let right_expression = self.parse_expression(precedence);
+
+        Expression::Infix(op, Box::new(left), Box::new(right_expression))
+    }
+
+    fn parse_prefix_expression(&mut self, pr: Prefix) -> Expression {
+        self.consume_token();
+
+        let expr = self.parse_expression(Precedence::Prefix);
+
+        Expression::Prefix(pr, Box::new(expr))
+    }
+
+    fn parse_grouped_expression(&mut self) -> Expression {
+        self.consume_token();
+
+        let exp = self.parse_expression(Precedence::Lowest);
+
+        if !self.expect_next_token(TokenType::RightParen) {
+            panic!("unexpected next token: TokenType::RightParen")
+        }
+
+        exp
+    }
+
+    fn current_precedence(&self) -> Precedence {
+        match self.current_token.kind {
+            TokenType::Eq => Precedence::Equals,
+            TokenType::NotEq => Precedence::Equals,
+            TokenType::LT => Precedence::LessGreater,
+            TokenType::GT => Precedence::LessGreater,
+            TokenType::PlusSign => Precedence::Sum,
+            TokenType::MinusSign => Precedence::Sum,
+            TokenType::SlashSign => Precedence::Product,
+            TokenType::MultiplicationSign => Precedence::Product,
+            TokenType::LeftParen => Precedence::Call,
+            _ => Precedence::Lowest,
+        }
+    }
+
+    fn next_precedence(&self) -> u8 {
+        match self.next_token.kind {
+            TokenType::Eq => Precedence::Equals as u8,
+            TokenType::NotEq => Precedence::Equals as u8,
+            TokenType::LT => Precedence::LessGreater as u8,
+            TokenType::GT => Precedence::LessGreater as u8,
+            TokenType::PlusSign => Precedence::Sum as u8,
+            TokenType::MinusSign => Precedence::Sum as u8,
+            TokenType::SlashSign => Precedence::Product as u8,
+            TokenType::MultiplicationSign => Precedence::Product as u8,
+            TokenType::LeftParen => Precedence::Call as u8,
+            _ => Precedence::Lowest as u8,
+        }
     }
 }
 
@@ -177,11 +263,11 @@ mod test {
         let myString = \"My string\";
         ";
         let expected = [
-            "Let(\"x\", Literal(Number(5)))",
-            "Let(\"y\", Literal(Number(100)))",
-            "Let(\"foobar\", Identifier(\"y\"))",
-            "Let(\"barfoo\", Literal(Boolean(false)))",
-            "Let(\"myString\", Literal(String(\"My string\")))",
+            "Let x Number (5)",
+            "Let y Number (100)",
+            "Let foobar Ident (y)",
+            "Let barfoo Bool (false)",
+            "Let myString String (My string)",
         ];
 
         let mut p = Parser::new(input);
@@ -189,7 +275,7 @@ mod test {
         let result = p.build_ast();
 
         for (i, curr) in result.statements.iter().enumerate() {
-            assert_eq!(format!("{:?}", curr), expected.get(i).unwrap().to_string());
+            assert_eq!(curr.to_string(), expected.get(i).unwrap().to_string());
         }
     }
 
@@ -202,11 +288,119 @@ mod test {
         ";
 
         let mut p = Parser::new(input);
-        let expected = ["return 5", "return 100", "return (+ foobar Int(2) 2)"];
+        let expected = [
+            "Return Number (5)",
+            "Return Number (100)",
+            "Return + Left Ident (foobar) | Right Number (2)",
+        ];
         let result = p.build_ast();
 
         for (i, curr) in result.statements.iter().enumerate() {
-            assert_eq!(format!("{:?}", curr), expected.get(i).unwrap().to_string());
+            assert_eq!(curr.to_string(), expected.get(i).unwrap().to_string());
+        }
+    }
+
+    #[test]
+    fn parse_prefix_expression() {
+        let input = "
+        !5;
+        -15;
+        !foobar;
+        -foobar;
+        !true;
+        !false;
+        5;
+        ";
+
+        let mut p = Parser::new(input);
+        let expected = [
+            "! Exp Number (5)",
+            "- Exp Number (15)",
+            "! Exp Ident (foobar)",
+            "- Exp Ident (foobar)",
+            "! Exp Bool (true)",
+            "! Exp Bool (false)",
+            "Number (5)",
+        ];
+
+        let result = p.build_ast();
+
+        for (i, curr) in result.statements.iter().enumerate() {
+            assert_eq!(curr.to_string(), expected.get(i).unwrap().to_string());
+        }
+    }
+
+    #[test]
+    fn parse_infix_expression() {
+        let input = "
+        5 + 5;
+        5 - 5;
+        5 * 5;
+        5 / 5;
+        5 > 5;
+        5 < 5;
+        5 == 5;
+        5 != 5;
+        foobar + foobar;
+        bar - bar;
+        bar * bar;
+        true == true;
+        false != true;
+        5 + 5 * 5;
+        -1 + 2;
+        a + b * c + d / e - f;
+        3 > 5 == false;
+        ";
+
+        let mut p = Parser::new(input);
+        let expected = [
+            "+ Left Number (5) | Right Number (5)",
+            "- Left Number (5) | Right Number (5)",
+            "* Left Number (5) | Right Number (5)",
+            "/ Left Number (5) | Right Number (5)",
+            "> Left Number (5) | Right Number (5)",
+            "< Left Number (5) | Right Number (5)",
+            "== Left Number (5) | Right Number (5)",
+            "!= Left Number (5) | Right Number (5)",
+            "+ Left Ident (foobar) | Right Ident (foobar)",
+            "- Left Ident (bar) | Right Ident (bar)",
+            "* Left Ident (bar) | Right Ident (bar)",
+            "== Left Bool (true) | Right Bool (true)",
+            "!= Left Bool (false) | Right Bool (true)",
+            "+ Left Number (5) | Right * Left Number (5) | Right Number (5)",
+            "+ Left - Exp Number (1) | Right Number (2)",
+            "- Left + Left + Left Ident (a) | Right * Left Ident (b) | Right Ident (c) | Right / Left Ident (d) | Right Ident (e) | Right Ident (f)",
+            "== Left > Left Number (3) | Right Number (5) | Right Bool (false)",
+        ];
+
+        let result = p.build_ast();
+
+        for (i, curr) in result.statements.iter().enumerate() {
+            assert_eq!(curr.to_string(), expected.get(i).unwrap().to_string());
+        }
+    }
+
+    #[test]
+    fn parse_grouped_expression() {
+        let input = "
+        1 + (2 + 3) + 4;
+        (5 + 5) * 2;
+        2 / (5 + 5);
+        -(5 + 5);
+        ";
+
+        let mut p = Parser::new(input);
+        let expected = [
+            "+ Left + Left Number (1) | Right + Left Number (2) | Right Number (3) | Right Number (4)",
+            "* Left + Left Number (5) | Right Number (5) | Right Number (2)",
+            "/ Left Number (2) | Right + Left Number (5) | Right Number (5)",
+            "- Exp + Left Number (5) | Right Number (5)",
+        ];
+
+        let result = p.build_ast();
+
+        for (i, curr) in result.statements.iter().enumerate() {
+            assert_eq!(curr.to_string(), expected.get(i).unwrap().to_string());
         }
     }
 }
--- src/ast/tree.rs
+++ src/ast/tree.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 type Identifier = String;
 
 pub struct Program {
@@ -11,10 +13,45 @@ pub enum Statement {
     Expression(Expression),
 }
 
+impl Display for Statement {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Statement::Let(identifier, exp) => write!(f, "Let {} {}", identifier, exp.to_string()),
+            Statement::Return(exp) => {
+                write!(f, "Return {}", exp.to_string())
+            }
+            Statement::Expression(exp) => {
+                write!(f, "{}", exp.to_string())
+            }
+        }
+    }
+}
+
 #[derive(Debug, Clone)]
 pub enum Expression {
     Literal(Literal),
     Identifier(Identifier),
+    Infix(Operator, Box<Expression>, Box<Expression>),
+    Prefix(Prefix, Box<Expression>),
+}
+
+impl Display for Expression {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Expression::Literal(Literal::Number(v)) => write!(f, "Number ({})", v),
+            Expression::Literal(Literal::String(s)) => write!(f, "String ({})", s),
+            Expression::Literal(Literal::Boolean(b)) => write!(f, "Bool ({})", b),
+            Expression::Identifier(i) => write!(f, "Ident ({})", i),
+            Expression::Infix(op, left, right) => write!(
+                f,
+                "{} Left {} | Right {}",
+                op.to_string(),
+                left.to_string(),
+                right.to_string()
+            ),
+            Expression::Prefix(pr, exp) => write!(f, "{} Exp {}", pr.to_string(), exp.to_string()),
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -22,4 +59,46 @@ pub enum Literal {
     Number(i64),
     String(String),
     Boolean(bool),
+}
+
+#[derive(Debug, Clone)]
+pub enum Operator {
+    Plus,
+    Minus,
+    Multiply,
+    Divide,
+    Equal,
+    NotEqual,
+    GreaterThan,
+    LessThan,
+}
+
+impl Display for Operator {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Operator::Plus => write!(f, "+"),
+            Operator::Minus => write!(f, "-"),
+            Operator::Multiply => write!(f, "*"),
+            Operator::Divide => write!(f, "/"),
+            Operator::Equal => write!(f, "=="),
+            Operator::NotEqual => write!(f, "!="),
+            Operator::GreaterThan => write!(f, ">"),
+            Operator::LessThan => write!(f, "<"),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum Prefix {
+    Bang,
+    Minus,
+}
+
+impl Display for Prefix {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Prefix::Bang => write!(f, "!"),
+            Prefix::Minus => write!(f, "-"),
+        }
+    }
 }
